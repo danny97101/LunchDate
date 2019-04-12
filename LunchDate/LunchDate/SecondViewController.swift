7deea34013c1d2cb627693adc9e6562d97e35749
Timothy Marotta <timothymarotta@Timothys-MacBook-Pro.local>
2019-04-12 11:45:18 -0400

    Initialized Xcode project

---
+++ LunchDate/LunchDate/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LunchDate
+//
+//  Created by Timothy Marotta on 4/12/19.
+//  Copyright © 2019 LunchDateTeam. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
