7727e67768b9600497a29e62e8bf0ec43eebeb5c
Timothy Marotta <timothymarotta@Timothys-MacBook-Pro.local>
2019-04-21 16:06:12 -0400

    More web scraping lol

--- WebScrape/SodexoScrape.py
+++ WebScrape/SodexoScrape.py
@@ -1,24 +1,37 @@
 
-#@author  Timothy Marotta
-#@date    April 18, 2019
-#@purpose Scrape food information from Sodexo Webpage
+# @author  Timothy Marotta
+# @date    April 18, 2019
+# @purpose Scrape food information from Sodexo Webpage
 
 from urllib.request import *
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
-import re
+import datetime
 
 
-def scrapesoup(scraped, url):
+def format(array):
+    array.remove('LUNCH')
+    array.remove('Chef Table')
+    array.remove('Deli')
+    array.remove('Food Lab')
+    array.remove('Pizza')
+    array.remove('Simple Servings')
+    array.remove('Soups')
+    return array
+
+def scrape_soup(scraped, url):
     html = urlopen(url).read().decode('UTF-8')
-
     soup = BeautifulSoup(html, 'html.parser')
-    #TODO need to select different div tag based on day, currently works for current - 5
-    #select bite date div tag instead, then lunch
-    temp = soup.find_all('div', class_="accordion-panel rtf hide")
-    print(temp[2])
+    day = datetime.datetime.today().day
+    menu = soup.find_all('div', id="menuid-"+str(day)+"-day")[-1].get_text("|", strip=True)
+    spliced = menu.split("|")
+
+    lunch_start = spliced.index('LUNCH')
+    lunch_end = spliced.index('DINNER')
+    display(format(spliced[lunch_start:lunch_end]))
 
 
+'''
 def scrape(scraped, url):
     try:
         data = urlopen(url)
@@ -33,20 +46,24 @@ def scrape(scraped, url):
             loc2 = lines[x].find('</a>')
             line = lines[x][loc + 2:loc2]
             scraped.append(str(x) + ': ' + line)
+'''
 
 
 def display(scraped):
-    for x in range(len(scraped)):
-        print(scraped[x]+', ')
+    for x in range(0, len(scraped)):
+        print(str(scraped[x])+', ')
 
 
 def main():
     campusCenter = []
     towers = []
     terraces = []
-    #scrape(terraces, "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=364&locationId=10001002&whereami=https://ithaca.sodexomyway.com/dining-near-me/terrace-dining-hall")
-    #scrape(campusCenter, "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=362&locationId=10001001&whereami=https://ithaca.sodexomyway.com/dining-near-me/campus-center-dining-hall")
-    scrapesoup(towers, "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=1356&locationId=10001003&whereami=http://ithaca.sodexomyway.com/dining-near-me/towers-dining-hall")
+    print("terraces")
+    scrape_soup(terraces, "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=364&locationId=10001002&whereami=https://ithaca.sodexomyway.com/dining-near-me/terrace-dining-hall")
+    # print("campus center")
+    #scrape_soup(campusCenter, "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=362&locationId=10001001&whereami=https://ithaca.sodexomyway.com/dining-near-me/campus-center-dining-hall")
+    # print("towers")
+    # scrape_soup(towers, "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=1356&locationId=10001003&whereami=http://ithaca.sodexomyway.com/dining-near-me/towers-dining-hall")
     #display(towers)
 
 
