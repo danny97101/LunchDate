a7dd62e35c222267e4a3fb2d00e7f8a1d7931567
Timothy Marotta <timothymarotta@Timothys-MacBook-Pro.local>
2019-04-22 18:13:55 -0400

    WebScrape functionality works

--- WebScrape/SodexoScrape.py
+++ WebScrape/SodexoScrape.py
@@ -9,17 +9,39 @@ from bs4 import BeautifulSoup
 import datetime
 
 
-def format(array):
-    array.remove('LUNCH')
-    array.remove('Chef Table')
-    array.remove('Deli')
-    array.remove('Food Lab')
-    array.remove('Pizza')
-    array.remove('Simple Servings')
-    array.remove('Soups')
-    return array
+def format_array(dh, array):
+    if dh == 0:
+        array.remove('LUNCH')
+        array.remove('Chef Table')
+        array.remove('Deli')
+        array.remove('Food Lab')
+        array.remove('Pizza')
+        array.remove('Simple Servings')
+        array.remove('Soups')
+        return array
+    elif dh == 1:
+        array.remove('LUNCH')
+        array.remove('Deli')
+        array.remove('Entrées')
+        array.remove('Grill')
+        array.remove('International')
+        array.remove('Pizza')
+        array.remove('Simple Servings')
+        array.remove('Soups')
+        array.remove('Vegan')
+        return array
+    elif dh == 2:
+        array.remove('LUNCH')
+        array.remove('Basil')
+        array.remove('Chef\'s Table')
+        array.remove('Daily Dish')
+        array.remove('Ignite')
+        return array
+    else:
+        print("Format unrecognized. Try again.")
 
-def scrape_soup(scraped, url):
+
+def scrape_soup(dh, scraped, url):
     html = urlopen(url).read().decode('UTF-8')
     soup = BeautifulSoup(html, 'html.parser')
     day = datetime.datetime.today().day
@@ -28,43 +50,24 @@ def scrape_soup(scraped, url):
 
     lunch_start = spliced.index('LUNCH')
     lunch_end = spliced.index('DINNER')
-    display(format(spliced[lunch_start:lunch_end]))
-
-
-'''
-def scrape(scraped, url):
-    try:
-        data = urlopen(url)
-    except HTTPError as e:
-        data = e.read()
-
-    lines = data.readlines()
-    for x in range(len(lines)):
-        lines[x] = lines[x].decode('utf-8')
-        if 'data-foodItemName' in lines[x] and x < 3600:
-            loc = lines[x].find("\">")
-            loc2 = lines[x].find('</a>')
-            line = lines[x][loc + 2:loc2]
-            scraped.append(str(x) + ': ' + line)
-'''
+    display(format_array(dh, spliced[lunch_start:lunch_end]))
 
 
 def display(scraped):
-    for x in range(0, len(scraped)):
-        print(str(scraped[x])+', ')
+    for x in range(0, len(scraped), 2):
+        print(str(scraped[x])+'\t'+str(scraped[x+1]))
 
 
 def main():
-    campusCenter = []
+    campus_center = []
     towers = []
     terraces = []
-    print("terraces")
-    scrape_soup(terraces, "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=364&locationId=10001002&whereami=https://ithaca.sodexomyway.com/dining-near-me/terrace-dining-hall")
-    # print("campus center")
-    #scrape_soup(campusCenter, "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=362&locationId=10001001&whereami=https://ithaca.sodexomyway.com/dining-near-me/campus-center-dining-hall")
-    # print("towers")
-    # scrape_soup(towers, "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=1356&locationId=10001003&whereami=http://ithaca.sodexomyway.com/dining-near-me/towers-dining-hall")
-    #display(towers)
+    print("----------terraces----------")
+    scrape_soup(0, terraces, "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=364&locationId=10001002&whereami=https://ithaca.sodexomyway.com/dining-near-me/terrace-dining-hall")
+    print("\n----------campus center----------")
+    scrape_soup(1, campus_center, "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=362&locationId=10001001&whereami=https://ithaca.sodexomyway.com/dining-near-me/campus-center-dining-hall")
+    print("\n----------towers----------")
+    scrape_soup(2, towers, "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=1356&locationId=10001003&whereami=http://ithaca.sodexomyway.com/dining-near-me/towers-dining-hall")
 
 
 main()
