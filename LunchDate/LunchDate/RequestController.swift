182d668b87ec9c87cc30ca03347d817b38d62555
Timothy Marotta <timothymarotta@Timothys-MacBook-Pro.local>
2019-04-29 21:36:48 -0400

    Added Controllers and nav bar icons

---
+++ LunchDate/LunchDate/RequestController.swift
@@ -0,0 +1,20 @@
+//
+//  RequestController.swift
+//  LunchDate
+//
+//  Created by Timothy Marotta on 4/29/19.
+//  Copyright © 2019 LunchDateTeam. All rights reserved.
+//
+
+import UIKit
+
+class RequestController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
--- LunchDate/LunchDate/SearchController.swift
+++ LunchDate/LunchDate/SearchController.swift
@@ -1,20 +1,20 @@
 //
-//  SecondViewController.swift
+//  SearchController.swift
 //  LunchDate
 //
-//  Created by Timothy Marotta on 4/12/19.
+//  Created by Timothy Marotta on 4/29/19.
 //  Copyright © 2019 LunchDateTeam. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SearchController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
