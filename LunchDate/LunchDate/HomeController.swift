2a64ac1b8f89864269df8f2f7c965c3cdc12c424
Timothy Marotta <timothymarotta@Timothys-MacBook-Pro.local>
2019-05-01 17:38:26 -0400

    sorry Danny lol

---
+++ LunchDate/LunchDate/HomeController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeController.swift
+//  LunchDate
+//
+//  Created by Timothy Marotta on 4/12/19.
+//  Copyright © 2019 LunchDateTeam. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
